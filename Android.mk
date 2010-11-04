7ee14e55003208748ba2368365dc4f454d61b273
Jason Chen <jasonchen@google.com>
2010-11-03 21:24:20 -0700

    Don't build Tag in master for now
    Change-Id: I550ab47ed5115d88a7fcbbfb2188a25a7596f87d

--- Android.mk
+++ Android.mk
@@ -6,13 +6,13 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_STATIC_JAVA_LIBRARIES := guava
 
 # Only compile source java files in this apk.
-LOCAL_SRC_FILES := $(call all-java-files-under, src)
+#LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_PACKAGE_NAME := Tag
 
-LOCAL_SDK_VERSION := current
+#LOCAL_SDK_VERSION := current
 
-include $(BUILD_PACKAGE)
+#include $(BUILD_PACKAGE)
 
 # Use the following include to make our test apk.
-include $(call all-makefiles-under,$(LOCAL_PATH))
+#include $(call all-makefiles-under,$(LOCAL_PATH))
