6c26b7c94f97f5c93200445488b1bb3fe5c542bc
Daisuke Miyakawa <dmiyakawa@google.com>
2011-03-03 18:34:40 -0800

    Use vcard lib
    Now the vcard code is available with LOCAL_SDK_VERSION=9.
    
    Change-Id: I8aad414263a2971c3052b231e20c6e1ff129d5ca

--- Android.mk
+++ Android.mk
@@ -3,7 +3,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_STATIC_JAVA_LIBRARIES := guava
+LOCAL_STATIC_JAVA_LIBRARIES := guava com.android.vcard
 
 # Only compile source java files in this apk.
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
